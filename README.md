a90a2e220a464a25d8d60a48f549eaa51627738a
HurayrahB <huray2007@gmail.com>
2026-07-30 15:46:48 -0400

    Played around with creating apps, urlconfigs, debugging tools

---
+++ README.md
@@ -0,0 +1,2 @@
+## Django Testing Project
+Learning the fundamentals of Django, nothing particularly functional
--- storefront/settings.py
+++ storefront/settings.py
@@ -36,9 +36,12 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "playground",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -78,6 +81,9 @@ DATABASES = {
     }
 }
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
